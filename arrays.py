bab14e1f1f0b7efe01e2e613917c9a1dc7a19635
rajathbrgowda <rajathbrgowda@gmail.com>
2023-08-07 14:52:32 -0400

    Leetcode 27

--- arrays.py
+++ arrays.py
@@ -20,4 +20,22 @@ class Solution:
 # Space complexity: O(1)
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+# 27. Remove Element [Easy]
+
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+# Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+# Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+# Return k.
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] != val:
+                nums[l] = nums[r]
+                l +=1
+        return l
+            
