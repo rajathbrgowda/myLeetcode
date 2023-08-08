f9fb7d8434e58b5bdf147c57a4ce3dcc356aab7a
rajathbrgowda <rajathbrgowda@gmail.com>
2023-08-08 11:17:39 -0400

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -1,9 +1,7 @@
 # 26. Remove Duplicates from Sorted Array [Easy]
 
 # Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
-
 # Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
-
 # Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
 # Return k.
 
@@ -25,11 +23,10 @@ class Solution:
 # 27. Remove Element [Easy]
 
 # Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
-
 # Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
-
 # Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
 # Return k.
+
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         l = 0
@@ -40,4 +37,23 @@ class Solution:
         return l
 
 # Time complexity: O(n)
-# Space complexity: O(1)            
+# Space complexity: O(1)    
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------        
+
+# 1929. Concatenation of Array  [Easy]
+
+# Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+# Specifically, ans is the concatenation of two nums arrays.
+# Return the array ans.
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = nums
+        for i in range(len(nums)):
+            ans.append(nums[i])
+        return ans
+
+# Time complexity: O(n)
+            
